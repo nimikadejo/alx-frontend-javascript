f8aef59880feebe8f5c98c809b4e425855c3628e
nimikadejo <kadejotoluwanimi@gmail.com>
2023-03-22 23:45:09 +0100

    get a promise funct

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,4 +1,3 @@
-function getResponseFromAPI() {
-	return new Promise((resolve, reject) => {
-		resolve('true')
-	})
+export default function getResponseFromAPI() {
+	return new Promise((resolve, reject) => [resolve, reject]);
+}
